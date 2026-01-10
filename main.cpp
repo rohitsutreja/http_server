81c827e40f86976b9297190e7bc634acc3b05982
rohitsutreja <rohitsutreja@gmail.com>
2026-01-10 16:26:31 +0000

    add router class

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 
 #include "Server.hpp"
+#include "Router.hpp"
 
 int main()
 {
     try
     {
-        Server server{8000};
+        http_server::Router router{};
+
+        router.Get("/", [](const HttpRequest &)
+                   {
+            HttpResponse res{};
+            res.status_code = 200;
+            res.status_message = "OK";
+            res.body = "<h1>Hello World</h1>";
+
+            res.headers.emplace("Content-Type", "text/html");
+            res.headers.emplace("Connection", "close");
+
+            return res; });
+
+        router.Get("/home", [](const HttpRequest &)
+                   {
+            HttpResponse res{};
+            res.status_code = 200;
+            res.status_message = "OK";
+            res.body = "<h1>Home!</h1>";
+
+            res.headers.emplace("Content-Type", "text/html");
+            res.headers.emplace("Connection", "close");
+
+            return res; });
+
+        http_server::Server server{8000};
+
+        server.set_handler([&router](const HttpRequest &req) -> HttpResponse
+                           { return router.route(req); });
+
         server.run();
     }
     catch (const std::exception &e)
